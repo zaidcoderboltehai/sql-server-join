6ab2c902432710896fa629118b5eab969fc330f5
zaidcoderboltehai <Pass@7488690>
2025-02-13 15:45:23 +0530

    Updated SQL queries

--- SQLJoin.sql
+++ SQLJoin.sql
@@ -1,3 +1,4 @@
+/*
 create database sqljoin
 
 use sqljoin
@@ -67,4 +68,74 @@ SELECT Students.student_id, Students.name, Students.class,
        Marks.mark_id, Marks.subject, Marks.marks
 FROM Students  
 CROSS JOIN Marks;
+*/
+
+CREATE DATABASE EmployeeDB;
+
+USE EmployeeDB;
+
+CREATE TABLE Employees (
+    emp_id INT PRIMARY KEY, 
+    emp_name VARCHAR(50), 
+    department VARCHAR(50)
+);
+
+INSERT INTO Employees (emp_id, emp_name, department)  
+VALUES 
+(1, 'Amit', 'IT'),  
+(2, 'Ravi', 'HR'),  
+(3, 'Sita', 'Finance'),  
+(4, 'Rohan', 'IT');
+
+CREATE TABLE Salaries (
+    salary_id INT IDENTITY(1,1) PRIMARY KEY,  
+    emp_id INT, 
+    salary_amount INT,
+    FOREIGN KEY (emp_id) REFERENCES Employees(emp_id)
+);
+
+INSERT INTO Salaries (emp_id, salary_amount)  
+VALUES  
+(1, 50000),  
+(2, 60000),  
+(4, 55000);
+
+SELECT Employees.emp_name, Employees.department, Salaries.salary_amount  
+FROM Employees  
+INNER JOIN Salaries  
+ON Employees.emp_id = Salaries.emp_id;
+
+
+SELECT Employees.emp_name, Employees.department, Salaries.salary_amount  
+FROM Employees  
+LEFT JOIN Salaries  
+ON Employees.emp_id = Salaries.emp_id;
+
+SELECT Employees.emp_name, Employees.department, Salaries.salary_amount  
+FROM Employees  
+RIGHT JOIN Salaries  
+ON Employees.emp_id = Salaries.emp_id;
+
+SELECT Employees.emp_name, Employees.department, Salaries.salary_amount  
+FROM Employees  
+FULL JOIN Salaries  
+ON Employees.emp_id = Salaries.emp_id;
+
+SELECT E1.emp_id AS Emp1_ID, 
+       E1.emp_name AS Emp1_Name, 
+       E2.emp_id AS Emp2_ID, 
+       E2.emp_name AS Emp2_Name, 
+       E1.department
+FROM Employees E1  
+JOIN Employees E2  
+ON E1.department = E2.department  
+AND E1.emp_id < E2.emp_id;
+
+SELECT Employees.emp_id, Employees.emp_name, Employees.department, 
+       Salaries.salary_id, Salaries.salary_amount
+FROM Employees  
+CROSS JOIN Salaries;
+
+
+
 
