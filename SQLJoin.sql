5d2380cec7d063144d3596b3d508f9cacf7a25ee
zaidcoderboltehai <Pass@7488690>
2025-02-14 22:48:09 +0530

    Updated SQL queries

--- SQLJoin.sql
+++ SQLJoin.sql
@@ -139,3 +139,54 @@ CROSS JOIN Salaries;
 
 
 
+CREATE TABLE Attendance (
+    attendance_id INT IDENTITY(1,1) PRIMARY KEY,
+    student_id INT,
+    status VARCHAR(10),  -- Present / Absent
+    date DATE,
+    FOREIGN KEY (student_id) REFERENCES Students(student_id)
+);
+
+INSERT INTO Attendance (student_id, status, date)  
+VALUES  
+(1, 'Present', '2024-02-01'),  
+(2, 'Absent', '2024-02-01'),  
+(3, 'Present', '2024-02-01');
+
+SELECT Students.name, Marks.subject, Marks.marks, Attendance.status, Attendance.date  
+FROM Students  
+INNER JOIN Marks ON Students.student_id = Marks.student_id  
+INNER JOIN Attendance ON Students.student_id = Attendance.student_id;
+
+SELECT Students.name, Marks.subject, Marks.marks, Attendance.status, Attendance.date  
+FROM Students  
+LEFT JOIN Marks ON Students.student_id = Marks.student_id  
+LEFT JOIN Attendance ON Students.student_id = Attendance.student_id;
+
+SELECT Students.name, Marks.subject, Marks.marks, Attendance.status, Attendance.date  
+FROM Students  
+RIGHT JOIN Marks ON Students.student_id = Marks.student_id  
+RIGHT JOIN Attendance ON Students.student_id = Attendance.student_id;
+
+SELECT Students.name, Marks.subject, Marks.marks, Attendance.status, Attendance.date  
+FROM Students  
+FULL JOIN Marks ON Students.student_id = Marks.student_id  
+FULL JOIN Attendance ON Students.student_id = Attendance.student_id;
+
+SELECT S1.student_id AS Student1_ID, 
+       S1.name AS Student1_Name, 
+       S2.student_id AS Student2_ID, 
+       S2.name AS Student2_Name, 
+       S1.class
+FROM Students S1  
+JOIN Students S2  
+ON S1.class = S2.class  
+AND S1.student_id < S2.student_id;
+
+SELECT Students.student_id, Students.name, Students.class, 
+       Marks.mark_id, Marks.subject, Marks.marks,
+       Attendance.attendance_id, Attendance.status, Attendance.date
+FROM Students  
+CROSS JOIN Marks  
+CROSS JOIN Attendance;
+
